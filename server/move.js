52a4d284b0a1be796cadd06a67d8d3cecce2a8fb
andisheh <andisheh.dysh.partovi@gmail.com>
2015-03-29 19:16:48 +1100

    Added skeleton and pseudocode for AI
    Added skeleton and pseudocode for AI. Also an outline of a move class
    that is very handy for the AI methods.

---
+++ server/move.js
@@ -0,0 +1,5 @@
+/*
+ * Move class to hold the data about a move
+ */
+//variable: cardID // the card that is going to be played in this move
+//variable: position // the board position that the card is going to be moved to
