ddb5799acbad9a4245f55c3c55304dad0e36e563
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2015-05-06 02:59:56 +1000

    Removed schemas

--- models/schemas.js
+++
@@ -1,9 +0,0 @@
-// Commented this out for  while
-// meteor-collection2 schemas for previously instantiated collections
-// var Schemas = {};
-
-// Schemas.Log = {};
-
-
-// Attach schemas to collections
-//GameLogs.attachSchema(Schemas.Log);
